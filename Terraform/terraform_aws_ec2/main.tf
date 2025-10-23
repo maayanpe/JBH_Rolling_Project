ee7bb318277c2d050e9ff14fe9df74c7033f69ab
maayanpe <68500551+maayanpe@users.noreply.github.com>
2025-10-23 20:35:36 +0300

    Remove IPv6 CIDR block from security group
    Removed IPv6 CIDR block from security group.

--- Terraform/terraform_aws_ec2/main.tf
+++ Terraform/terraform_aws_ec2/main.tf
@@ -69,7 +69,6 @@ resource "aws_security_group" "builder_sg" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = { Name = "builder-sg" }
